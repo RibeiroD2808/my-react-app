8ceace60ece5c052929f845f326668d7709ffec5
RibeiroD2808 <diogo.ribeiro.2808@gmail.com>
2024-01-03 21:30:31 +0000

    create week div \n organize code to put all api calls in one place \n css updates

--- src/App.js
+++ src/App.js
@@ -6,35 +6,52 @@ function App() {
   
   //api response
   const [data, setData] = useState(0);
-  const handleSearchData = (searchData) => {
-    
+  const [forecast, setForecast] = useState(0);
+
+  const handleSearchData = (searchData) => {  
     setData(searchData);
   };
 
+  const handleUpdateForecast = (forecast) => {
+    setForecast(forecast);
+  };
+
+
   //default data
-  if(!data){
+  if(data == 0){
     
    /*  fetch('https://api.openweathermap.org/data/2.5/weather?lat=41.4417677&lon=-8.2955712&appid=' + apiKey + '&units=metric')
     .then(response => response.json())
     .then(data => handleSearchData(data))
     */
     const data = {"coord":{"lon":10.99,"lat":44.34},"weather":[{"id":501,"main":"Rain","description":"moderate rain","icon":"10d"}],"base":"stations","main":{"temp":298.48,"feels_like":298.74,"temp_min":297.56,"temp_max":300.05,"pressure":1015,"humidity":64,"sea_level":1015,"grnd_level":933},"visibility":10000,"wind":{"speed":0.62,"deg":349,"gust":1.18},"rain":{"1h":3.16},"clouds":{"all":100},"dt":1661870592,"sys":{"type":2,"id":2075663,"country":"IT","sunrise":1661834187,"sunset":1661882248},"timezone":7200,"id":3163858,"name":"Zocca","cod":200};
-    handleSearchData(data);    
+    handleSearchData(data);
+    const forecast = {"cod":"200","message":0,"cnt":40,"list":[{"dt":1647345600,"main":{"temp":287.39,"feels_like":286.38,"temp_min":286.69,"temp_max":287.39,"pressure":1021,"sea_level":1021,"grnd_level":1018,"humidity":58,"temp_kf":0.7},"weather":[{"id":803,"main":"Clear","description":"broken clouds","icon":"04d"}],"clouds":{"all":71},"wind":{"speed":3.08,"deg":128,"gust":4.3},"visibility":10000,"pop":0,"sys":{"pod":"d"},"dt_txt":"2022-03-15 15:00:0"},{"dt":1647356400,"main":{"temp":287.09,"feels_like":286.13,"temp_min":286.5,"temp_max":287.09,"pressure":1021,"sea_level":1021,"grnd_level":1016,"humidity":61,"temp_kf":0.59},"weather":[{"id":803,"main":"Rain","description":"broken clouds","icon":"04d"}],"clouds":{"all":81},"wind":{"speed":3.28,"deg":168,"gust":3.96},"visibility":10000,"pop":0,"sys":{"pod":"d"},"dt_txt":"2022-03-15 15:00:00"},{"dt":1647367200,"main":{"temp":285.44,"feels_like":284.6,"temp_min":284.47,"temp_max":285.44,"pressure":1020,"sea_level":1020,"grnd_level":1016,"humidity":72,"temp_kf":0.97},"weather":[{"id":804,"main":"Snow","description":"overcast clouds","icon":"04d"}],"clouds":{"all":90},"wind":{"speed":2.7,"deg":183,"gust":5.59},"visibility":10000,"pop":0,"sys":{"pod":"d"},"dt_txt":"2022-03-15 15:00:00"},{"dt":1647766800,"main":{"temp":282.42,"feels_like":280,"temp_min":282.42,"temp_max":282.42,"pressure":1036,"sea_level":1036,"grnd_level":1033,"humidity":60,"temp_kf":0},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"}],"clouds":{"all":39},"wind":{"speed":4.58,"deg":83,"gust":8.45},"visibility":10000,"pop":0,"sys":{"pod":"d"},"dt_txt":"2022-03-20 15:00:00"}],"city":{"id":2643743,"name":"London","coord":{"lat":51.5085,"lon":-0.1257},"country":"GB","population":1000000,"timezone":0,"sunrise":1647324902,"sunset":1647367441}};    
+    handleUpdateForecast(forecast);
+
   }
+
+  console.log(data);
+  console.log(forecast);
 
   return (
     <div id='gridDiv'>
-      <SearchBar apiKey = {apiKey} onSearch={handleSearchData}/>  
-      {data ? <Day data = {data}/> : null}
-      {data ? <Week data = {data}/> : null}
-      {/*data ? <Maps data = {//data}/> : null*/}
+      <SearchBar apiKey = {apiKey} onSearch={handleSearchData} updateForecast={handleUpdateForecast}/>  
+      <div id='infoDiv'>
+        {data ? <Day data = {data}/> : null}
+        {forecast ? <Week data = {forecast}/> : null}
+        {/*data ? <Maps data = {//data}/> : null*/}
+      </div>
     </div>
   );
 }
 
-function SearchBar({apiKey, onSearch}){
+function SearchBar({apiKey, onSearch, updateForecast}){
   
   const [searchText, setSearchText] = useState('');
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
+  
 
   //set input value
   const handleChange = e => {
@@ -48,15 +65,22 @@ function SearchBar({apiKey, onSearch}){
     fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + searchText + '&appid=' + apiKey)
     .then(response => response.json())
     .then( async data => {
-      console.log(data);
 
       //current weather
       const weatherResponse = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + data[0].lat + '&lon=' + data[0].lon + '&appid=' + apiKey + '&units=metric');
       const weatherData = await weatherResponse.json();
+
+      setLat(data[0].lat);
+      setLon(data[0].lon);
       //update data
       onSearch(weatherData);
 
-    })
+    }).then(fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey)
+      .then(response => response.json())        
+      .then(data => {      
+            updateForecast(data);
+      })
+    )
                      
     return;
     
@@ -74,7 +98,6 @@ function SearchBar({apiKey, onSearch}){
 function Day({data}){
 
   
-  console.log(data);
   const weather = data.weather[0].main;
   const degrees = data.main.temp;
   const minDegrees = data.main.temp_min;
@@ -83,22 +106,19 @@ function Day({data}){
   const windSpeed = data.wind.speed;
 
   let imgName = "/WheatherImages.jpg";
-
-  console.log(weather);
-  console.log(imgName);
   
   switch (weather){
     case 'Rain':
-      imgName = "/WheatherImages.jpg"
+      imgName = "/Rain.png"
       break;
     case 'Clear':
-      imgName = "/WheatherImages.jpg"
+      imgName = "/Clear.png"
       break;
     case 'Clouds':
-      imgName = "/WheatherImages.jpg"
+      imgName = "/Clouds.png"
       break;
     case 'Snow':
-      imgName = "/WheatherImages.jpg"
+      imgName = "/Snow.png"
       break;
   }
   
@@ -112,7 +132,7 @@ function Day({data}){
     const options = {month: 'short' };
     const formattedDate = date.toLocaleString('en-US', options);
 
-    return <p> {date.getDay()} {formattedDate}, {date.getHours()} : {date.getMinutes()} </p>
+    return <p id='dateNow'> {date.getDay()} {formattedDate}, {date.getHours()} : {date.getMinutes()} </p>
     
   }
 
@@ -136,83 +156,29 @@ function Day({data}){
   )
 }
 
-function Week({apiKey ,data}){
+function Week({data}){
   
-  //const [forecast, setForecast] = useState([]);
-  const lat = data.coord.lat;
-  const lon = data.coord.lon;
-  
-  const fakeData = ({"cod":"200","message":0,"cnt":40,"list":[{"dt":1647345600,"main":{"temp":287.39,"feels_like":286.38,"temp_min":286.69,"temp_max":287.39,"pressure":1021,"sea_level":1021,"grnd_level":1018,"humidity":58,"temp_kf":0.7},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"clouds":{"all":71},"wind":{"speed":3.08,"deg":128,"gust":4.3},"visibility":10000,"pop":0,"sys":{"pod":"d"},"dt_txt":"2022-03-15 12:00:00"},{"dt":1647356400,"main":{"temp":287.09,"feels_like":286.13,"temp_min":286.5,"temp_max":287.09,"pressure":1021,"sea_level":1021,"grnd_level":1016,"humidity":61,"temp_kf":0.59},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"clouds":{"all":81},"wind":{"speed":3.28,"deg":168,"gust":3.96},"visibility":10000,"pop":0,"sys":{"pod":"d"},"dt_txt":"2022-03-15 15:00:00"},{"dt":1647367200,"main":{"temp":285.44,"feels_like":284.6,"temp_min":284.47,"temp_max":285.44,"pressure":1020,"sea_level":1020,"grnd_level":1016,"humidity":72,"temp_kf":0.97},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"clouds":{"all":90},"wind":{"speed":2.7,"deg":183,"gust":5.59},"visibility":10000,"pop":0,"sys":{"pod":"d"},"dt_txt":"2022-03-15 18:00:00"},{"dt":1647766800,"main":{"temp":282.42,"feels_like":280,"temp_min":282.42,"temp_max":282.42,"pressure":1036,"sea_level":1036,"grnd_level":1033,"humidity":60,"temp_kf":0},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"}],"clouds":{"all":39},"wind":{"speed":4.58,"deg":83,"gust":8.45},"visibility":10000,"pop":0,"sys":{"pod":"d"},"dt_txt":"2022-03-20 09:00:00"}],"city":{"id":2643743,"name":"London","coord":{"lat":51.5085,"lon":-0.1257},"country":"GB","population":1000000,"timezone":0,"sunrise":1647324902,"sunset":1647367441}});
-  
-  let forecast = fakeData.list.filter(element =>{
+
+  //slice data text and add to array if the data is 15pm
+  const forecast = data.list.filter(element =>{
     return element.dt_txt.slice(11, 13) === '15'; 
     })
-  forecast.shift();
-  console.log(forecast);
-  /* 
-  fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey)
-  .then(response => response.json())
-  .then(data => {
-    //slice data text and add to array if the data is 15pm
-     const forecast = data.list.filter(element =>{
-      return element.dt_txt.slice(11, 13) === '15'; 
-      })
-    forecast.shift();
-    setForecast(forecast);
-  });
- */
+  //forecast.shift();
+
+  const listDays = forecast.map((day) =>
+    <li>
+
+      <img src={day.weather[0].main + ".png"}   alt={day.weather[0].main + ".png"}></img>
+
+    </li>
+  );
+
   return(
     <div id="weekDiv">
-    
+      <ul>{listDays}</ul>
     </div>
   )
 }
 
-function Maps(data){
-  
-  function Map({layer}){
-    const src = 'https://tile.openweathermap.org/map/' + layer + '/1/1/1.png?appid=9b4b60468dfddcc0882aa6b9937b9a7c';
-    return <img id="weatherImg" src={src} alt={layer + 'Map'} />
-  }
-
-  return(
-    <div id='mapsDiv'>
-      <Map layer="temp_new"/>
-      <Map layer="clouds_new"/>
-    </div>
-  )
-}
-
-//https://openweathermap.org/api/weathermaps
-function Map(layer, z, x, y, apikey){
-  
-  
-  return(
-    <div>
-      
-    </div>
-  )
-}
-
-function ApiTest() {
-  
-  const [data, setData] = useState([]);
-
-  fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
-  .then(response => response.json())
-  .then(data => setData(data));
-
-
-  return (
-    <div className='div'>
-       {data.map(post => (
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </div>
-      ))}
-    </div>
-  )
-}
 
 export default App;
